1051a437aec279c2dc02dd15cedbd1a61bdebce6
MeltingDiamond <adrian.oestrem@gmail.com>
2025-12-06 14:27:30 +0100

    Made background automatically place itself in the correct place using code

--- Assets/Scripts/Fish/FishSpawner.cs
+++ Assets/Scripts/Fish/FishSpawner.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 public class FishSpawner : MonoBehaviour
 {
     public List<GameObject> zone1FishPrefabs;
+    public int maxAmountInZone1;
+    private List<GameObject> _zone1Spawns;
+    
     public List<GameObject> zone2FishPrefabs;
+    public int maxAmountInZone2;
+    private List<GameObject> _zone2Spawns;
+    
     public List<GameObject> zone3FishPrefabs;
+    public int maxAmountInZone3;
+    private List<GameObject> _zone3Spawns;
     
     void Start()
     {
@@ -14,9 +22,31 @@ public class FishSpawner : MonoBehaviour
 
     void Update()
     {
+        //if ((_zone1Spawns.Count < maxAmountInZone1))
+        //{
+          //  var fish = Instantiate(zone1FishPrefabs[ChooseFishToSpawn(zone1FishPrefabs)], GenerateRandomSpawnPos());
+            //_zone1Spawns.Add(fish);
+        //}
         
+        //if ((_zone2Spawns.Count < maxAmountInZone2))
+        //{
+          //  var fish = Instantiate(zone2FishPrefabs[ChooseFishToSpawn(zone2FishPrefabs)], GenerateRandomSpawnPos());
+            //_zone2Spawns.Add(fish);
+        //}
+        
+       // if ((_zone3Spawns.Count < maxAmountInZone3))
+        //{
+         //   var fish = Instantiate(zone3FishPrefabs[ChooseFishToSpawn(zone3FishPrefabs)], GenerateRandomSpawnPos());
+           // _zone3Spawns.Add(fish);
+        //}
     }
 
+    private Vector2 GenerateRandomSpawnPos(Vector2 topLeftCorner, Vector2 bottomRightCorner)
+    {
+        var x = Random.Range(bottomRightCorner.x, topLeftCorner.x);
+        var y = Random.Range(bottomRightCorner.y, bottomRightCorner.y);
+        return new Vector2(x, y);
+    }
     private int ChooseFishToSpawn(List<GameObject> list)
     {
         int choice = Random.Range( 0, list.Count -1);
--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,6 +1,21 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public GameObject zone1bg;
+    public GameObject zone2bg;
+    public GameObject zone3bg;
+    
+    private void Start()
+    {
+        zone1bg.transform.position = Vector3.zero;
+        var bg1Size = zone1bg.GetComponent<SpriteRenderer>().size;
+        
+        zone2bg.transform.position = new Vector3(0, bg1Size.y / 2, 0);
+        var bg2Size = zone2bg.GetComponent<SpriteRenderer>().size;
+        
+        zone3bg.transform.position = new Vector3(0, bg2Size.y / 2, 0);
+        var bg3Size = zone3bg.GetComponent<SpriteRenderer>().size;
+    }
 }
